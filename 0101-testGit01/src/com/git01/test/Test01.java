467ce05f98510e2746b4862d1d8ffe60f5731293
zdl1 <1925159557@qq.com>
2021-08-08 13:17:06 +0800

    new project for test git

---
+++ 0101-testGit01/src/com/git01/test/Test01.java
@@ -0,0 +1,18 @@
+package com.git01.test;
+
+/**
+ * 
+ * @author 张登琳  TEL：13810218664 QQ:1925159557
+ * @since 2021年8月8日 下午1:04:58
+ * @version 1.0.0
+ */
+public class Test01 {
+	void m() {
+		System.out.println("Test01.m()");
+	};
+	
+	int n() {
+		System.out.println("enclosing_type.enclosing_method()");
+		return 1;
+	}
+}
